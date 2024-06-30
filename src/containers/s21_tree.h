1bc790701c84ce7c5179c02c6cbb0cbd87be3cfc
risahamm <risahamm@student.21-school.ru>
2024-06-30 21:51:01 +0300

    AVL TREE Added: --Comparator: gcc собирает. --FindInsert: корректно обрабатывает неуникальные значения;

--- src/containers/s21_tree.h
+++ src/containers/s21_tree.h
@@ -75,7 +75,6 @@ class Tree {
       root_->parent = nullptr;
       root_->key = new_key;
       root_->data = value;
-      std::cout << &root_->left << std::endl;
     } else {
       ret_val = FindInsert(root_, new_key, value);
     }
@@ -102,46 +101,11 @@ class Tree {
   size_t size_;  // number of elements in the tree
 
   /* searches for the right place to insert a new node and inserts */
-  //  bool FindInsert(Node<KeyType, ValueType> *root, const key_type new_key,
-  //                  const mapped_type value) {
-  //    if (new_key == root->key) {
-  //      return false;
-  //    }
-  //    bool ret_val = true;
-  //    auto *child = new Node<KeyType, ValueType>;
-  //    if (new_key > root->key) {
-  //      if (root->right != nullptr) {
-  //        ret_val = FindInsert(root->right, new_key, value);
-  //      } else {
-  //        root->right = child;
-  //      }
-  //    } else {
-  //      if (root->left != nullptr) {
-  //        ret_val = FindInsert(root->left, new_key, value);
-  //      } else {
-  //        root->left = child;
-  //      }
-  //    }
-  //    if (ret_val) {
-  //      child->key = new_key;
-  //      child->data = value;
-  //      child->parent = root;
-  //      if (root->left == nullptr || root->right == nullptr) {
-  //        UpdateHeight(child->parent);
-  //      }
-  //    }
-  //    //    else {
-  //    //      child.destroy(); // TODO потом добавить деструктор
-  //    //    }
-  //    return ret_val;
-  //  }
-
-  /* searches for the right place to insert a new node and inserts */
   bool FindInsert(Node<KeyType, ValueType> *root, const key_type new_key,
                   const mapped_type value) {
 
     bool ret_val = true;
-    Node<KeyType, ValueType> **direction = Comparator(root, new_key); // = new Node<KeyType, ValueType>;
+    Node<KeyType, ValueType> **direction = Comparator(&root, new_key); // = new Node<KeyType, ValueType>;
     if (*direction == root) {
       return false;
     }
@@ -157,33 +121,24 @@ class Tree {
         UpdateHeight(child->parent);
       }
     }
-//    if (ret_val) {
-//      child->key = new_key;
-//      child->data = value;
-//      child->parent = root;
-//      if (root->left == nullptr || root->right == nullptr) {
-//        UpdateHeight(child->parent);
-//      }
-//    }
-    //    else {
-    //      child.destroy(); // TODO потом добавить деструктор
-    //    }
     return ret_val;
   }
 
   /* compares the keys and returns an iterator to a left or right child. if the
    * key is equal to the current node key, returns an iterator to the current
    * node */
-  Node<KeyType, ValueType> **Comparator(Node<KeyType, ValueType> *root, const key_type new_key) {
-    if (new_key == root->key) {
-      return &root;
+  Node<KeyType, ValueType> **Comparator(Node<KeyType, ValueType> **root, const key_type new_key) {
+    Node<KeyType, ValueType> **result; ;
+    if (new_key == (*root)->key) {
+      result = root;
     }
-    else if (new_key > root->key) {
-      return &root->right;
+    else if (new_key > (*root)->key) {
+      result = &(*root)->right;
     }
-    else if (new_key < root->key) {
-      return &root->left;
+    else if (new_key < (*root)->key) {
+      result = &(*root)->left;
     }
+    return result;
   }
 
   /* recursively updates the height of the branch */
