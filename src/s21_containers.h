6b9bf2ca32a1ef39b6bbcb7cfe674f95dc1345f7
risahamm <risahamm@student.21-school.ru>
2024-08-09 21:08:17 +0300

    MAP, SET, MULTISET done

--- src/s21_containers.h
+++ src/s21_containers.h
@@ -3,14 +3,13 @@
 
 #include <iostream>
 
-#include "containers/s21_map.h"
-#include "containers/s21_set.h"
-#include "containers/s21_multiset.h"
-#include "containers/s21_vector.h"
-#include "containers/s21_tree.h"
-#include "containers/s21_list.h"
-#include "containers/s21_queue.h"
-#include "containers/s21_stack.h"
 #include "containers/s21_array.h"
+#include "containers/s21_list.h"
+#include "containers/s21_map.h"
+#include "containers/s21_multiset.h"
+#include "containers/s21_queue.h"
+#include "containers/s21_set.h"
+#include "containers/s21_stack.h"
+#include "containers/s21_vector.h"
 
 #endif  // S21_CONTAINERS_SRC_S21_CONTAINERS_H_
